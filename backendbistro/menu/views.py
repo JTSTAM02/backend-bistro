a197e3473634d6b5aed5bd6083050a3881566857
root <tyler.stamm1313@gmail.com>
2023-07-27 21:34:33 +0000

    Adding comments for clarity

--- backendbistro/menu/views.py
+++ backendbistro/menu/views.py
@@ -1,24 +1,26 @@
-from django.shortcuts import render
 from django.http import JsonResponse
 from .models import Menu_Items
 
 def get_menu_items(request):
+    # use 'select_related' to get 'category' and 'cuisine' data
     menu_items = Menu_Items.objects.select_related("category","cuisine").all()
     data = []
     for item in menu_items:
-        data.append({
-            "id": item.id,
+        data.append({ # creates dictionary that has menu items attributes
+            "id": item.id, # realized too late I didn't need this
             "title": item.title,
             "description": item.description,
             "price": item.price,
             "spice_level": item.spice_level,
             "category": item.category.name,
-            "cuisine": item.category.name, 
+            "cuisine": item.cuisine.name, 
             "location": {
                 "id": item.location_id,
                 "name": item.location.name,
             }
 
             })
+        # JsonResponse turns data into JSON
+        # 'safe=False' needed because data parameter is a list, not a dictionary
     return JsonResponse(data, safe=False)
     
